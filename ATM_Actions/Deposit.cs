f8fc5c0176dc9a4c6f1d35d351a1a891526e9a52
Microsft-Wrd <60941772+MicrosoftWord12@users.noreply.github.com>
2021-09-15 11:49:03 +0100

    Modified: Deposit.cs - It now works with database It takes out money from data so your bank account is lower

--- ATM_Actions/Deposit.cs
+++ ATM_Actions/Deposit.cs
@@ -6,6 +6,22 @@ namespace Console_ATM_Project.ATM_Actions
 {
     class Deposit
     {
-        // This is to simulate depositing something into the bank etc
+        public int DepositAmount(int cardNumber, int cardPin)
+        {
+            int currentBalance = Database.SelectCardBalance.CheckBalance(cardNumber, cardPin);
+            int withdrawAmount = int.Parse(Console.ReadLine());
+            int newAmount = currentBalance + withdrawAmount;
+
+            try
+            {
+                String SQL = $"update useraccount set user_amount='{newAmount}' where='{cardNumber}'";
+                return int.Parse(Database.DBCommand.command(SQL).ExecuteScalar().ToString());
+            }catch(Exception e)
+            {
+                 Console.WriteLine(e.ToString());
+                 return 0;
+             }
+
+        }
     }
 }
